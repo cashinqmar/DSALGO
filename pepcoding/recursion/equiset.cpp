0d9063b1b4ed085dcb92060ed8b3b30e2feed6da
Sachin Kumar <sachink061198@gmail.com>
2020-01-08 12:34:09 +0530

    commit on master

---
+++ pepcoding/recursion/equiset.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int equiset(vector<int>& arr,int sum1,int sum2,string s1,string s2,int idx){
+    if(idx==arr.size()){
+        if(sum1==sum2){
+            cout<<s1<<" "<<" = "<<s2<<endl;
+            return 1;
+        }
+        return 0;
+    }
+    int count=0;
+    count+=equiset(arr,sum1+arr[idx],sum2,s1 +" "+to_string(arr[idx])+" ",s2,idx+1);
+    count+=equiset(arr,sum1,sum2+arr[idx],s1,s2+" "+to_string(arr[idx])+" ",idx+1);
+
+    return count;
+}
+
+int main(){
+vector<int> arr={10,20,30,40,50,60,70,80};
+cout<<equiset(arr,0,0,"","",0);
+}
